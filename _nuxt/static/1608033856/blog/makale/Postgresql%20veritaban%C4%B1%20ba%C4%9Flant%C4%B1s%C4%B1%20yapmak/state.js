ba428d116036c4c28977ebb023848f88ce166368
erdemaytek <31161840+erdemaytek@users.noreply.github.com>
2020-12-15 12:04:22 +0000

    Deploying to main from  @ 8f562b63623c44901119a2021e48678d70d46289 🚀

---
+++ _nuxt/static/1608033856/blog/makale/Postgresql%20veritaban%C4%B1%20ba%C4%9Flant%C4%B1s%C4%B1%20yapmak/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1608033856",layout:"default",error:null,serverRendered:true,routePath:"\u002Fblog\u002Fmakale\u002FPostgresql%20veritaban%C4%B1%20ba%C4%9Flant%C4%B1s%C4%B1%20yapmak",config:{content:{dbHash:"d703cca3"}}};
